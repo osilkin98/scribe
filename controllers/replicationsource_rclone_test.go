816adf4e7f7cee09dc3b101cbd2f65c84d0e80fc
Oleg Silkin <o.silkin98@gmail.com>
2021-06-14 17:05:34 -0400

    adds secret & second test case
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- controllers/replicationsource_rclone_test.go
+++ controllers/replicationsource_rclone_test.go
@@ -7,6 +7,9 @@ import (
 
 	"context"
 	"fmt"
+	batchv1 "k8s.io/api/batch/v1"
+	"k8s.io/apimachinery/pkg/types"
+
 	// snapv1 "github.com/kubernetes-csi/external-snapshotter/client/v4/apis/volumesnapshot/v1beta1"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -26,7 +29,13 @@ var _ = Describe("ReplicationSource [rclone]", func() {
 	var namespace *corev1.Namespace
 	var rs *scribev1alpha1.ReplicationSource
 	var srcPVC *corev1.PersistentVolumeClaim
+	var rcloneSecret *corev1.Secret
 	srcPVCCapacity := resource.MustParse("7Gi")
+
+	// dummy variables taken from https://scribe-replication.readthedocs.io/en/latest/usage/rclone/index.html#source-configuration
+	var configSection = "foo"
+	var destPath = "bar"
+	//var config = "foobar"
 	//logger := zap.New(zap.UseDevMode(true), zap.WriteTo(GinkgoWriter))
 	//logger.
 
@@ -55,6 +64,16 @@ var _ = Describe("ReplicationSource [rclone]", func() {
 			},
 		}
 
+		rcloneSecret = &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "rclone-secret",
+				Namespace: namespace.Name,
+			},
+			StringData: map[string]string{
+				"rclone.conf": "hunter2",
+			},
+		}
+
 		rs = &scribev1alpha1.ReplicationSource{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "instance",
@@ -65,7 +84,6 @@ var _ = Describe("ReplicationSource [rclone]", func() {
 			},
 		}
 		RcloneContainerImage = DefaultRcloneContainerImage
-		fmt.Printf("rs: %+v\n", rs)
 	})
 	AfterEach(func() {
 		// delete each namespace on shutdown so resources can be reclaimed
@@ -74,6 +92,7 @@ var _ = Describe("ReplicationSource [rclone]", func() {
 	JustBeforeEach(func() {
 		// source pvc comes up
 		Expect(k8sClient.Create(ctx, srcPVC)).To(Succeed())
+		Expect(k8sClient.Create(ctx, rcloneSecret)).To(Succeed())
 		Expect(k8sClient.Create(ctx, rs)).To(Succeed())
 		// wait for the ReplicationSource to actually come up
 		Eventually(func() error {
@@ -85,10 +104,6 @@ var _ = Describe("ReplicationSource [rclone]", func() {
 	Context("when a schedule is not specified", func() {
 		fmt.Printf("Replication Source %+v\n", rs)
 		BeforeEach(func() {
-			// dummy variables taken from https://scribe-replication.readthedocs.io/en/latest/usage/rclone/index.html#source-configuration
-			var configSection = "foo"
-			var destPath = "bar"
-			var config = "foobar"
 			// changing this block from Rsync to Rclone causes the unit
 			rs.Spec.Rclone = &scribev1alpha1.ReplicationSourceRcloneSpec{
 				ReplicationSourceVolumeOptions: scribev1alpha1.ReplicationSourceVolumeOptions{
@@ -96,7 +111,7 @@ var _ = Describe("ReplicationSource [rclone]", func() {
 				},
 				RcloneConfigSection: &configSection,
 				RcloneDestPath:      &destPath,
-				RcloneConfig:        &config,
+				RcloneConfig:        &rcloneSecret.Name,
 			}
 		})
 		// we should not be syncing again if no schedule is specified
@@ -111,4 +126,43 @@ var _ = Describe("ReplicationSource [rclone]", func() {
 			}, duration, interval).Should(BeFalse())
 		})
 	})
+
+	Context("When a copyMethod of None is specified for Rclone", func() {
+		BeforeEach(func() {
+			rs.Spec.Rclone = &scribev1alpha1.ReplicationSourceRcloneSpec{
+				ReplicationSourceVolumeOptions: scribev1alpha1.ReplicationSourceVolumeOptions{
+					CopyMethod: scribev1alpha1.CopyMethodNone,
+				},
+				RcloneConfigSection: &configSection,
+				RcloneDestPath:      &destPath,
+				RcloneConfig:        &rcloneSecret.Name,
+			}
+		})
+
+		It("Uses the Source PVC as the sync source", func() {
+			job := &batchv1.Job{}
+			Eventually(func() error {
+				fmt.Printf("ReplicationSource so far: %+v\n", rs)
+				return k8sClient.Get(ctx, types.NamespacedName{Name: "scribe-rclone-src-" + rs.Name, Namespace: rs.Namespace}, job)
+			}, maxWait, interval).Should(Succeed())
+			volumes := job.Spec.Template.Spec.Volumes
+			found := false
+			for _, v := range volumes {
+				if v.PersistentVolumeClaim != nil && v.PersistentVolumeClaim.ClaimName == srcPVC.Name {
+					found = true
+				}
+			}
+			Expect(found).To(BeTrue())
+			Expect(srcPVC).NotTo(beOwnedBy(rs))
+		})
+	})
+
+	//Context("When the rclone secret isn't specified", func() {
+	//	BeforeEach(func() {
+	//		var rcloneSecret = "foobar"
+	//		rs.Spec.Rclone = {
+	//
+	//		}
+	//	})
+	//})
 })
