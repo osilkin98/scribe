41d32b968cfcdb388592c5a78aa7ac6f1f586ae1
Oleg Silkin <o.silkin98@gmail.com>
2021-06-15 16:21:43 -0400

    adds rclone testing for ReplicationSource
    Signed-off-by: Oleg Silkin <o.silkin98@gmail.com>

--- controllers/replicationsource_rclone_test.go
+++ controllers/replicationsource_rclone_test.go
@@ -7,12 +7,14 @@ import (
 
 	"context"
 	"fmt"
+
 	batchv1 "k8s.io/api/batch/v1"
 	"k8s.io/apimachinery/pkg/types"
 
 	// snapv1 "github.com/kubernetes-csi/external-snapshotter/client/v4/apis/volumesnapshot/v1beta1"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
 	// "github.com/operator-framework/operator-lib/status"
 	//batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -63,7 +65,7 @@ var _ = Describe("ReplicationSource [rclone]", func() {
 				},
 			},
 		}
-
+		// need secret for most of these tests to work
 		rcloneSecret = &corev1.Secret{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "rclone-secret",
@@ -154,6 +156,127 @@ var _ = Describe("ReplicationSource [rclone]", func() {
 			}
 			Expect(found).To(BeTrue())
 			Expect(srcPVC).NotTo(beOwnedBy(rs))
+		})
+	})
+
+	Context("when a copyMethod of Clone is specified", func() {
+		BeforeEach(func() {
+			rs.Spec.Rclone = &scribev1alpha1.ReplicationSourceRcloneSpec{
+				ReplicationSourceVolumeOptions: scribev1alpha1.ReplicationSourceVolumeOptions{
+					CopyMethod: scribev1alpha1.CopyMethodClone,
+				},
+				RcloneConfigSection: &configSection,
+				RcloneDestPath:      &destPath,
+				RcloneConfig:        &rcloneSecret.Name,
+			}
+		})
+
+		// attempts to invoke rloneReconcile
+		//nolint:dupl
+		It("creates a clone of the source PVC as the sync source", func() {
+			job := &batchv1.Job{}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, types.NamespacedName{Name: "scribe-rclone-src-" + rs.Name, Namespace: rs.Namespace}, job)
+			}, maxWait, interval).Should(Succeed())
+			volumes := job.Spec.Template.Spec.Volumes
+			pvc := &corev1.PersistentVolumeClaim{}
+			pvc.Namespace = rs.Namespace
+			found := false
+			for _, v := range volumes {
+				if v.PersistentVolumeClaim != nil {
+					found = true
+					pvc.Name = v.PersistentVolumeClaim.ClaimName
+				}
+			}
+			Expect(found).To(BeTrue())
+			Expect(k8sClient.Get(ctx, nameFor(pvc), pvc)).To(Succeed())
+			Expect(pvc.Spec.DataSource.Name).To(Equal(srcPVC.Name))
+			Expect(pvc).To(beOwnedBy(rs))
+
+		})
+	})
+
+	// todo: test for sync job being paused
+	// taken after this: https://github.com/backube/scribe/blob/e81281ac0fc1d50c3ae31369495f3c5fe3927a8f/controllers/replicationsource_test.go#L426
+	Context("Pausing an rclone sync job", func() {
+		parallelism := int32(0)
+		BeforeEach(func() {
+			rs.Spec.Paused = true
+			rs.Spec.Rclone = &scribev1alpha1.ReplicationSourceRcloneSpec{
+				ReplicationSourceVolumeOptions: scribev1alpha1.ReplicationSourceVolumeOptions{
+					CopyMethod: scribev1alpha1.CopyMethodClone,
+				},
+				RcloneConfigSection: &configSection,
+				RcloneDestPath:      &destPath,
+				RcloneConfig:        &rcloneSecret.Name,
+			}
+		})
+		It("job will be created but won't start", func() {
+			job := &batchv1.Job{}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, types.NamespacedName{Name: "scribe-rclone-src-" + rs.Name, Namespace: rs.Namespace}, job)
+			}, maxWait, interval).Should(Succeed())
+			Expect(*job.Spec.Parallelism).To(Equal(parallelism))
+		})
+	})
+
+	// todo: test rcloneSrcReconciler.cleanupJob
+	//
+
+	// todo: test invalid rclone Spec
+	// we want to target a job
+	Context("When rclone fails to validate the spec", func() {
+		var emptyString = ""
+		BeforeEach(func() {
+			rs.Spec.Rclone = &scribev1alpha1.ReplicationSourceRcloneSpec{
+				ReplicationSourceVolumeOptions: scribev1alpha1.ReplicationSourceVolumeOptions{
+					CopyMethod: scribev1alpha1.CopyMethodNone,
+				},
+				RcloneConfig:        &emptyString,
+				RcloneConfigSection: &emptyString,
+				RcloneDestPath:      &emptyString,
+			}
+		})
+		It("No spec at all", func() {
+			job := &batchv1.Job{}
+			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: "scribe-rclone-src-" + rs.Name, Namespace: rs.Namespace}, job)).NotTo(Succeed())
+		})
+
+	})
+
+	Context("rclone has secret but nothing else", func() {
+		BeforeEach(func() {
+			var emptyString = ""
+			rs.Spec.Rclone = &scribev1alpha1.ReplicationSourceRcloneSpec{
+				ReplicationSourceVolumeOptions: scribev1alpha1.ReplicationSourceVolumeOptions{
+					CopyMethod: scribev1alpha1.CopyMethodNone,
+				},
+				RcloneConfig:        &rcloneSecret.Name,
+				RcloneConfigSection: &emptyString,
+				RcloneDestPath:      &emptyString,
+			}
+		})
+		It("running rclone with a broken config", func() {
+			job := &batchv1.Job{}
+			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: "scribe-rclone-src-" + rs.Name, Namespace: rs.Namespace}, job)).NotTo(Succeed())
+		})
+	})
+
+	Context("rclone has secret + rcloneConfigSection but not DestPath", func() {
+		BeforeEach(func() {
+			var emptyString = ""
+			rs.Spec.Rclone = &scribev1alpha1.ReplicationSourceRcloneSpec{
+				ReplicationSourceVolumeOptions: scribev1alpha1.ReplicationSourceVolumeOptions{
+					CopyMethod: scribev1alpha1.CopyMethodNone,
+				},
+				RcloneConfig:        &rcloneSecret.Name,
+				RcloneConfigSection: &configSection,
+				RcloneDestPath:      &emptyString,
+			}
+		})
+		It("Existing RcloneConfig + RcloneConfigSection", func() {
+			job := &batchv1.Job{}
+			Expect(k8sClient.Get(ctx, types.NamespacedName{Name: "scribe-rclone-src-" + rs.Name, Namespace: rs.Namespace}, job)).NotTo(Succeed())
 		})
 	})
 
